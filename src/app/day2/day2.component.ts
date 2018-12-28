dfbe134a99cff42e3d977fba7ca73cc0f2dcfd1f
Sushmitha DT <dtsushmitha@gmail.com>
2018-12-28 23:58:17 +0530

    third commit

--- src/app/day2/day2.component.ts
+++ src/app/day2/day2.component.ts
@@ -9,7 +9,44 @@ export class Day2Component implements OnInit {
 
   superHeros = ["Superman", "Batman", "Wonder woman", "Flash"]; // *ngFor
 
-  num: String='0';
+  myColor: String = 'pink'; //[ngStyle] mapping property to a variable
+
+  num: String = '0'; // *ngSwitch
+
+  oddOrEven: String = 'Odd or Even?'; //[ngStyle]
+  oddOrEvenColor: String = 'pink';
+
+  cities: any[] = [
+    { 'city': 'Delhi', 'country': 'India' },
+    { 'city': 'New York', 'country': 'USA' },
+    { 'city': 'Sydney', 'country': 'Australia' },
+    { 'city': 'Bengaluru', 'country': 'India' },
+    { 'city': 'London', 'country': 'UK' },
+    { 'city': 'Canberra', 'country': 'Australia' },
+    { 'city': 'San-Fransico', 'country': 'USA' }
+  ];
+
+  getCityColor(city: any) {
+    switch (city) {
+      case 'India': return 'turquoise';
+      case 'USA': return 'red';
+      case 'Australia': return 'yellow';
+      case 'UK': return 'blue';
+    }
+  }
+
+  oddOrEvenFun(num: number) {
+    if (num % 2 == 0) {
+      this.oddOrEven = 'Even!';
+      this.oddOrEvenColor = 'aqua'
+    }
+    else {
+      this.oddOrEven = 'Odd!';
+      this.oddOrEvenColor = 'greenyellow';
+    }
+  }
+
+
 
   constructor() { }
 
